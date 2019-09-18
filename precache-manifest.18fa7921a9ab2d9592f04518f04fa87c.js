1d1a98edf4bb6b98e25c1b9075b62a9ee6536f0e
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-09-18 15:00:46 +0200

    Updates

---
+++ precache-manifest.18fa7921a9ab2d9592f04518f04fa87c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555715965e3df31b7086c23237bae1c1",
+    "url": "/siren/index.html"
+  },
+  {
+    "revision": "e54eb7f391db5f117688",
+    "url": "/siren/static/css/main.3bdaec13.chunk.css"
+  },
+  {
+    "revision": "e75ec15c8476b4b6cc07",
+    "url": "/siren/static/js/2.c9119ef4.chunk.js"
+  },
+  {
+    "revision": "e54eb7f391db5f117688",
+    "url": "/siren/static/js/main.d8c44c26.chunk.js"
+  },
+  {
+    "revision": "6b1faaa73d702aaa2883",
+    "url": "/siren/static/js/runtime~main.1eaf5f67.js"
+  }
+]);
